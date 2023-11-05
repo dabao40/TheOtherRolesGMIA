4429b080a4a3df09c5b728516c24710bed1a4ed1
dabaoImp11 <dabaoliu2019@163.com>
2023-11-05 16:57:59 +0800

    Fixed Custom Hats

---
+++ TheOtherRoles/Modules/CustomHats/Helpers.cs
@@ -0,0 +1,25 @@
+using System.IO;
+using UnityEngine;
+
+namespace TheOtherRoles.Modules.CustomHats;
+
+internal static class Helpers
+{
+    public static Texture2D LoadTextureFromPath(string path)
+    {
+        if (!File.Exists(path)) return null;
+        var texture = new Texture2D(2, 2, TextureFormat.ARGB32, true);
+        try
+        {
+            var byteTexture = Il2CppSystem.IO.File.ReadAllBytes(path);
+            ImageConversion.LoadImage(texture, byteTexture, false);
+        }
+        catch
+        {
+            TheOtherRolesPlugin.Logger.LogError("Error loading texture from disk: " + path);
+            return null;
+        }
+
+        return texture;
+    }
+}
