4f4e954a880a3204b541079f07d3bfca7adea56f
SaskueUchiwa <150168532+SaskueUchiwa@users.noreply.github.com>
2024-01-13 14:51:01 +0000

    New Utility Color Gradient

--- TheOtherRoles/Helpers.cs
+++ TheOtherRoles/Helpers.cs
@@ -166,6 +166,55 @@ namespace TheOtherRoles {
             AmongUsClient.Instance.FinishRpcImmediately(writer);
             RPCProcedure.trapperMeetingFlag();
         }
+        // Intersteing Color Gradient Feature :)
+        public static string GradientColorText(string startColorHex, string endColorHex, string text)
+        {
+           if (startColorHex.Length != 6 || endColorHex.Length != 6)
+           {
+              TheOtherRolesPlugin.Logger.LogError("GradientColorText : Invalid Color Hex Code, Hex code should be 6 characters long (without #) (e.g., FFFFFF).");
+              return text;
+           } 
+
+           Color startColor = HexToColor(startColorHex);
+           Color endColor = HexToColor(endColorHex);
+
+            int textLength = text.Length;
+            float stepR = (endColor.r - startColor.r) / (float)textLength;
+            float stepG = (endColor.g - startColor.g) / (float)textLength;
+            float stepB = (endColor.b - startColor.b) / (float)textLength;
+            float stepA = (endColor.a - startColor.a) / (float)textLength;
+
+            string gradientText = "";
+
+            for (int i = 0;  i < textLength; i++)
+            {
+                float r = startColor.r + (stepR * i);
+                float g = startColor.g + (stepG * i);
+                float b = startColor.b + (stepB * i);
+                float a = startColor.a + (stepA * i);
+
+
+                string colorhex = ColorToHex(new Color(r,g,b, a));
+                gradientText += $"<color=#{colorhex}>{text[i]}</color>";
+
+            }
+
+           return gradientText;
+
+        }
+
+        private static Color HexToColor(string hex)
+        {
+            Color color = new ();
+            ColorUtility.TryParseHtmlString("#" + hex, out color);
+            return color;
+        }
+
+        private static string ColorToHex(Color color)
+        {
+            Color32 color32 = (Color32)color;
+            return $"{color32.r:X2}{color32.g:X2}{color32.b:X2}{color32.a:X2}";
+        }
 
         public static void enableCursor(bool initalSetCursor)
         {
